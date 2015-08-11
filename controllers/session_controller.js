885c776480c74a4eb114a476d4f7019f281083c2
amilian <mln@gmx.com>
2015-08-11 07:26:36 +0200

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,4 +1,13 @@
-﻿exports.new = function (req, res) {
+﻿exports.loginRequired = function (req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+
+exports.new = function (req, res) {
     var errors = req.session.errors || {};
     req.session.errors = {};
     
